90b7c51d2bcca79c25c568ccac53a9352217f414
Alexis Covarrubias Gutiérrez <acovarrubias@grupoinndex.com.mx>
2026-09-25 15:25:13 -0600

    Provocando error deliberado para evidencia

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 const { sumar } = require('../src/index');
 
 test('suma 2 + 3 = 5', () => {
-  expect(sumar(2, 3)).toBe(5);
+  expect(sumar(2, 3)).toBe(999);
 });
 
 test('suma -1 + 1 = 0', () => {
